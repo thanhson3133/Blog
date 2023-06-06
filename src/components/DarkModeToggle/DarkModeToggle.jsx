6fe26161b66730e4b4fe237344f5709c3f53bb1e
thanhson3133 <thanhson3133@gmail.com>
2023-06-06 16:43:24 +0700

    update css dark mode

--- src/components/DarkModeToggle/DarkModeToggle.jsx
+++ src/components/DarkModeToggle/DarkModeToggle.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useContext } from "react";
-import styles from "./darkmodetoggle.module.css";
+import styles from "./darkModeToggle.module.css";
 import { ThemeContext } from "../../context/ThemeContext";
 
 const DarkModeToggle = () => {
